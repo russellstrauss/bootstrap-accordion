871f0493efd2dba94fc0ad3c7abda2026a26b506
Russell Strauss <rstrauss@agencyoasis.com>
2017-01-04 12:43:10 -0500

    Initial check in.

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+(function () {
+	// For accordions with long content, scroll to the top of the content after opening
+	// $("#accordion").on("shown.bs.collapse", function () {
+	// 	var el = $(this).find('.collapse.in').closest('.panel');
+	// 	$('html, body').scrollTop($(el).offset().top);
+	// });
+
+})();
