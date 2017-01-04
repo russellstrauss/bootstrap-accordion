be5e4980b05c359336902de8e27679d6a0bf5b35
Russell Strauss <rstrauss@agencyoasis.com>
2017-01-04 16:15:48 -0500

    Adding scrollTo function with animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,13 @@
 	// 	var el = $(this).find('.collapse.in').closest('.panel');
 	// 	$('html, body').scrollTop($(el).offset().top);
 	// });
+	
+	// Or the same feature as above, but with animation
+	// $("#accordion").on("shown.bs.collapse", function () {
+	// 	var myEl = $(this).find('.collapse.in').closest('.panel');
 
+	// 	$('html, body').animate({
+	// 		scrollTop: $(myEl).offset().top
+	// 	}, 500);
+	// });
 })();
